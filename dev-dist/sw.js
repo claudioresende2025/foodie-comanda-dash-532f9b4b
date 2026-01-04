46a4f735f22da7f5f27dd8101b7d38e15fed9d20
claudioresende2025 <claudinhoresendemoura@gmail.com>
2026-01-04 06:30:54 +0000

    Auto-sync: 2026-01-04 06:30:54

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.35u1u17tl18"
+    "revision": "0.6r1m8qgr3t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
