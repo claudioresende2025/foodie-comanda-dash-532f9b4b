2f540aea8ea53eb5e4137e3931fecd4babaa4be4
claudioresende2025 <claudinhoresendemoura@gmail.com>
2026-01-13 15:10:32 -0300

    fuction para reembolso

--- supabase/functions/request-refund/index.ts
+++ supabase/functions/request-refund/index.ts
@@ -15,6 +15,7 @@ const getErrorMessage = (e: unknown): string => {
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
+  "Access-Control-Allow-Methods": "POST, OPTIONS",
 };
 
 serve(async (req: Request) => {
