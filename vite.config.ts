c7f0f29edc4d342af14ccfc9dd8a5bde5ad0be8b
claudioresende2025 <claudinhoresendemoura@gmail.com>
2026-01-15 09:04:29 -0300

    autosync: update 2026-01-15T12:04:29.726Z

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(async ({ mode }) => {
   const plugins: PluginOption[] = [
     react(),
     VitePWA({
-      registerType: "prompt",
+      registerType: "autoUpdate",
       includeAssets: ["pwa-icon.png", "pwa-icon-192.png", "pwa-icon-512.png", "apple-touch-icon.png"],
       devOptions: {
         enabled: true
@@ -62,7 +62,7 @@ export default defineConfig(async ({ mode }) => {
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2,jpg,jpeg,webp}"],
         cleanupOutdatedCaches: true,
-        skipWaiting: false,
+        skipWaiting: true,
         clientsClaim: true,
         runtimeCaching: [
           {
